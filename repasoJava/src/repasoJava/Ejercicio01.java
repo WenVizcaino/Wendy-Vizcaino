aba0b3c49016ff2054bca7b4472bdaa97875ee30
APRENDIZ <APRENDIZ@STMAPRRMDFSP611.FormacionSena.RED>
2025-02-12 18:28:09 -0500

    Initial commint

---
+++ repasoJava/src/repasoJava/Ejercicio01.java
@@ -0,0 +1,20 @@
+package repasoJava;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+	
+    var lector = new Scanner(System.in);
+    
+    System.out.println("Nombre: ");
+    var nombre = lector.nextLine();
+    
+    System.out.println("Nombre: " + nombre);
+    
+    
+	}
+	
+
+}
---
+++ repasoJava/src/repasoJava/OperadorTernario.java
@@ -0,0 +1,22 @@
+package repasoJava;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class OperadorTernario {
+
+	public static void main(String[] args) {
+	Scanner lt = new Scanner(System.in);
+	int not = 0;
+	int AcumNot = 0;
+
+	//System.out.println("*** Operador Tenario ***");
+	int cantNot = 3;
+	for (int i = 1; i <= cantNot; i++) {
+		
+	System.out.println("Dijite la nota #" +i+":");
+	not=lt.nextInt();
+	
+	 }
+	}
+}
